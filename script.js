bb0e0ba437f6a4be938574a8e15addd90377e6f6
animesh3640 <127398605+animesh3640@users.noreply.github.com>
2023-05-02 21:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  // loop through half of the string and compare characters
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = palindrome
